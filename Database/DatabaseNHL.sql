6e7ce7c837c7e6cabf05cce3eb4f635e20daa643
Mehdi <khouchmehdi@gmail.com>
2024-05-27 01:15:22 +0200

    Update Teams

--- Database/DatabaseNHL.sql
+++ Database/DatabaseNHL.sql
@@ -1,33 +1,72 @@
 USE nhl;
-CREATE TABLE BostonBruins (
-    PlayerID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(25),
-    LastName VARCHAR(25),
-    DateOfBirth DATE,
-    Team VARCHAR(25),
-    Country VARCHAR(25),
-    Position VARCHAR(25),
-    Salary DECIMAL(10, 2),
-    DateExpiration DATE
+CREATE TABLE franchises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    city VARCHAR(25) NOT NULL,
+    state VARCHAR(30),
+    country VARCHAR(20) NOT NULL, 
+    Salary_Cap int
 );
-Select * FROM BostonBruins;
-INSERT INTO BostonBruins (FirstName, LastName, DateOfBirth, Team, Country, Position, Salary, DateExpiration)
-VALUES ('David', 'Pastrnak', "1996-05-25", "Boston Bruins", "Czech Republic", "RW", 11250000, "2031-06-30"); 
-INSERT INTO BostonBruins (FirstName, LastName, DateOfBirth, Team, Country, Position, Salary, DateExpiration)
-VALUES 
-('Brad', 'Marchand', "1988-05-11", "Boston Bruins", 'Canada', 'LW', 6125000, '2025-06-30'), 
-('Charlie', 'Coyle', '1992-03-02', 'Boston Bruins', 'USA', 'C', 5250000, '2026-06-30'),
-('Pavel', 'Zacha', '1997-04-06', 'Boston Bruins', "Czech Republic", 'C', 4750000, '2027-06-30'),
-('Trent', 'Frederic', '1998-02-11', 'Boston Bruins', 'USA', 'LW', 2300000, '2025-06-30'),
-('Morgan', 'Geekie', '1998-07-20', 'Boston Bruins', 'Canada', 'RW', 2000000, '2025-06-30'),
-('John', 'Beecher', '2001-04-05', 'Boston Bruins', 'USA', 'C', 925000, '2025-06-30'), 
-('Patrick', 'Brown', '1992-05-29', 'Boston Bruins', 'USA', 'C', 800000, '2025-06-30'),
-('Jakub', 'Lauko', '2000-03-28', 'Boston Bruins', "Czech Republic", "RW", 787500, '2025-06-30'),
-('Justin', 'Brazeau', '1998-02-02', 'Boston Bruins', 'Canada', 'RW', 775000, '2025-06-30');
-Select * FROM BostonBruins;
 
+ALTER TABLE franchises
+ADD COLUMN conference ENUM('Est', 'West'),
+ADD COLUMN division ENUM('Atlantic', 'Metropolitan', 'Central', 'Pacific');
 
+CREATE TABLE players (
+    player_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    age INT,
+    position ENUM('F', 'W', 'D', 'G') NOT NULL,
+    contract_end_date DATE,
+    contract_value DECIMAL(15, 2),
+    free_agent_status ENUM('UFA', 'RFA') DEFAULT NULL,
+    franchise_id INT,
+    FOREIGN KEY (franchise_id) REFERENCES franchises(id)
+);
+CREATE TABLE free_agents (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    age INT,
+    position ENUM('F', 'W', 'D', 'G') NOT NULL,
+    free_agent_status ENUM('UFA', 'RFA') NOT NULL
+);
 
+INSERT INTO franchises (name, city, state, country, conference, division) VALUES 
+('Boston Bruins', 'Boston', 'MA', 'USA', 'Est', 'Atlantic'), 
+('Toronto Maple Leafs', 'Toronto', 'ON', 'Canada', 'Est', 'Atlantic'), 
+('Montreal Canadiens', 'Montreal', 'QC', 'Canada', 'Est', 'Atlantic'), 
+('Buffalo Sabres', 'Buffalo', 'NY', 'USA', 'Est', 'Atlantic'), 
+('Detroit Red Wings', 'Detroit', 'MI', 'USA', 'Est', 'Atlantic'), 
+('Florida Panthers', 'Sunrise', 'FL', 'USA', 'Est', 'Atlantic'), 
+('Ottawa Senators', 'Ottawa', 'ON', 'Canada', 'Est', 'Atlantic'), 
+('Tampa Bay Lightning', 'Tampa', 'FL', 'USA', 'Est', 'Atlantic'), 
+('Carolina Hurricanes', 'Raleigh', 'NC', 'USA', 'Est', 'Metropolitan'), 
+('Columbus Blue Jackets', 'Columbus','OH', 'USA', 'Est', 'Metropolitan'), 
+('New Jersey Devils', 'Newark', 'NJ', 'USA', 'Est', 'Metropolitan'), 
+('New York Islanders', 'Uniondale', 'NY', 'USA', 'Est', 'Metropolitan'), 
+('New York Rangers', 'New York', 'NY', 'USA', 'Est', 'Metropolitan'), 
+('Philadelphia Flyers', 'Philadelphia', 'PA', 'USA', 'Est', 'Metropolitan'), 
+('Pittsburgh Penguins', 'Pittsburgh', 'PA', 'USA', 'Est', 'Metropolitan'), 
+('Washington Capitals', 'Washington', 'DC', 'USA', 'Est', 'Metropolitan');
 
-
-
+INSERT INTO franchises (name, city, state, country, conference, division) VALUES 
+('Chicago Blackhawks', 'Chicago', 'IL', 'USA', 'West', 'Central'), 
+('Colorado Avalanche', 'Denver', 'CO', 'USA', 'West', 'Central'), 
+('Dallas Stars', 'Dallas', 'TX', 'USA', 'West', 'Central'), 
+('Minnesota Wild', 'Saint Paul', 'MN', 'USA', 'West', 'Central'), 
+('Nashville Predators', 'Nashville', 'TN', 'USA', 'West', 'Central'), 
+('St. Louis Blues', 'St. Louis', 'MO', 'USA', 'West', 'Central'), 
+('Winnipeg Jets', 'Winnipeg', 'MB', 'Canada', 'West', 'Central'), 
+('Anaheim Ducks', 'Anaheim', 'CA', 'USA', 'West', 'Pacific'), 
+('Arizona Coyotes', 'Glendale', 'AZ', 'USA', 'West', 'Pacific'), 
+('Calgary Flames', 'Calgary', 'AB', 'Canada', 'West', 'Pacific'), 
+('Edmonton Oilers', 'Edmonton', 'AB', 'Canada', 'West', 'Pacific'), 
+('Los Angeles Kings', 'Los Angeles', 'CA', 'USA', 'West', 'Pacific'), 
+('San Jose Sharks', 'San Jose', 'CA', 'USA', 'West', 'Pacific'), 
+('Seattle Kraken', 'Seattle', 'WA', 'USA', 'West', 'Pacific'), 
+('Vancouver Canucks', 'Vancouver', 'BC', 'Canada', 'West', 'Pacific'), 
+('Vegas Golden Knights', 'Paradise', 'NV', 'USA', 'West', 'Pacific');
